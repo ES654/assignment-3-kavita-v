cfe50b280f476824fbffd27f8e0573853132215d
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-02-21 21:13:28 +0530

    Update q9_dataset.py

--- q9_dataset.py
+++ q9_dataset.py
@@ -2,3 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from linearRegression.linearRegression import LinearRegression
+# from linearRegression.linearRegression import *
+from metrics import *
+
+np.random.seed(42)
+
+N = 30
+P = 5
+X = pd.DataFrame(np.random.randn(N, P))
+z = X[0]*4 + X[2]*3
+y = pd.Series(np.random.randn(N))
+X['z'] = z
+
+for fit_intercept in [True, False]:
+    LR = LinearRegression(fit_intercept=fit_intercept)
+    LR.fit_autograd(X, y, batch_size=7, lr_type='inverse') # here you can use fit_non_vectorised / fit_autograd methods
+    y_hat = LR.predict(X)
+    print('For fit_intercept = '+str(fit_intercept))
+    print('RMSE: ', rmse(y_hat, y))
+    print('MAE: ', mae(y_hat, y))
